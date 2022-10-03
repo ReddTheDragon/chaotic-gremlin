792e8674267fa0c5455bb91b3c88a78f8433883d
Thomas <36747151+ReddTheDragon@users.noreply.github.com>
2022-10-03 19:33:21 -0400

    Update owner.py

--- checks/owner.py
+++ checks/owner.py
@@ -2,25 +2,6 @@ import discord
 from discord.ext import commands
 import asyncio,random
 def isowner(ctx):
-    async def predicate(ctx):
-        if isinstance(ctx,discord.Interaction):
-            if ctx.user.id == 653787366548570123:
-                return True
-            return False
-        else:
-            if ctx.author.user.id == 653787366548570123:
-                return True
-            return False
-    return commands.check(predicate)
-
+    return ctx.message.author.id == 653787366548570123
 def isowner_slash(ctx):
-    async def predicate(ctx):
-        if isinstance(ctx,discord.Interaction):
-            if ctx.user.id == 653787366548570123:
-                return True
-            return False
-        else:
-            if ctx.author.user.id == 653787366548570123:
-                return True
-            return False
-    return discord.app_commands.check(predicate)
+    return ctx.user.id == 653787366548570123
