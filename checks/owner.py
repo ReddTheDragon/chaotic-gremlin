8486d1928134f533de7249cfb03dcbdf8ac637e3
Thomas <36747151+ReddTheDragon@users.noreply.github.com>
2022-10-03 18:44:45 -0400

    Update owner.py
    Actually distinguishes between checks for slash commands and bot commands

--- checks/owner.py
+++ checks/owner.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 import asyncio,random
-def isowner():
+def isowner(ctx):
     async def predicate(ctx):
         if isinstance(ctx,discord.Interaction):
             if ctx.user.id == 653787366548570123:
@@ -11,3 +11,16 @@ def isowner():
             if ctx.author.user.id == 653787366548570123:
                 return True
             return False
+    return commands.check(predicate)
+
+def isowner_slash(ctx):
+    async def predicate(ctx):
+        if isinstance(ctx,discord.Interaction):
+            if ctx.user.id == 653787366548570123:
+                return True
+            return False
+        else:
+            if ctx.author.user.id == 653787366548570123:
+                return True
+            return False
+    return discord.app_commands.check(predicate)
